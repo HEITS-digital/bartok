5d489e8340748a88ec175158fba7e3c94cfa660c
Catalin <ctlnmhl@gmail.com>
2024-03-08 16:04:04 +0200

    Replaced escaped underscores resulted by converting html text into string

--- internal/service/happy_birthday.go
+++ internal/service/happy_birthday.go
@@ -2,10 +2,12 @@ package service
 
 import (
 	"bartok/internal/datastruct"
+	"os"
+	"strings"
+	"time"
+
 	md "github.com/JohannesKaufmann/html-to-markdown"
 	"github.com/slack-go/slack"
-	"os"
-	"time"
 )
 
 type HappyBirthdayService interface {
@@ -24,6 +26,9 @@ func (w *happyBirthdayService) PostBirthDayCards() []datastruct.EmployeeEvent {
 	for _, event := range events {
 		if len(event.Text) > 0 {
 			markdown, _ := converter.ConvertString(event.Text)
+			if len(markdown) > 0 {
+				markdown = strings.ReplaceAll(markdown, `\_`, `_`)
+			}
 			err := w.slackService.SendMessage(channelId, []slack.Block{
 				textToTextWithMrkdwnBlock(markdown),
 			})
